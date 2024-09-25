ad4c6e48ce9e81579162c3aecade310b0f5c4143
mujtaba ahmad <mmujtabach1234@gmail.com>
2024-09-25 21:33:22 +0500

    Final Commit

--- model.py
+++ model.py
@@ -7,7 +7,6 @@ from sklearn.linear_model import LogisticRegression
 data = load_iris()
 X, y = data.data, data.target
 
-# Split data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train a logistic regression model
